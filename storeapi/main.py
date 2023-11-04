00da56e051d07dd5a7df93540d9fcb442826ced3
Tommy Han <tommy.han@ownbackup.com>
2023-11-04 12:33:33 -0400

    initial version

---
+++ storeapi/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+# BaseModel is using for type declaration
+class UserPostIn(BaseModel):
+    body: str
+
+
+class UserPost(UserPostIn):
+    id: int
+
+
+post_table = {}
+
+
+@app.post("/post", response_model=UserPost)
+async def create_post(post: UserPostIn):
+    data = post.dict()
+    last_record_id = len(post_table)
+    new_post = {**data, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
+
+
+@app.get("/post", response_model=list[UserPost])
+async def get_all_posts():
+    return list(post_table.values())
