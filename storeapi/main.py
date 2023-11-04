38b3c5afd0447a83528fa30fa0bc7874f04a2563
Tommy Han <tommy.han@ownbackup.com>
2023-11-04 13:00:53 -0400

    added in router

--- storeapi/main.py
+++ storeapi/main.py
@@ -1,19 +1,6 @@
 from fastapi import FastAPI
-from storeapi.models.post import UserPost, UserPostIn
+from storeapi.routers.post import router as post_router
 
 app = FastAPI()
-post_table = {}
 
-
-@app.post("/post", response_model=UserPost)
-async def create_post(post: UserPostIn):
-    data = post.dict()
-    last_record_id = len(post_table)
-    new_post = {**data, "id": last_record_id}
-    post_table[last_record_id] = new_post
-    return new_post
-
-
-@app.get("/post", response_model=list[UserPost])
-async def get_all_posts():
-    return list(post_table.values())
+app.include_router(post_router)
---
+++ storeapi/routers/post.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+from storeapi.models.post import UserPost, UserPostIn
+
+
+router = APIRouter()
+post_table = {}
+
+
+@router.post("/post", response_model=UserPost)
+async def create_post(post: UserPostIn):
+    data = post.dict()
+    last_record_id = len(post_table)
+    new_post = {**data, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
+
+
+@router.get("/post", response_model=list[UserPost])
+async def get_all_posts():
+    return list(post_table.values())
