38b5ee137f6dcff04d6b992f133ff952364a6f19
Tommy Han <tommy.han@ownbackup.com>
2023-12-23 23:25:14 -0500

    add comments

--- storeapi/models/post.py
+++ storeapi/models/post.py
@@ -7,3 +7,23 @@ class UserPostIn(BaseModel):
 
 class UserPost(UserPostIn):
     id: int
+
+
+class CommentIn(BaseModel):
+    body: str
+    post_id: int
+
+
+class Comment(CommentIn):
+    id: int
+
+
+class UserPostWithComments(BaseModel):
+    post: UserPost
+    comments: list[Comment]
+
+
+# {
+#     "post": { "id": 0, "body": "My Post"},
+#     "comments": [{"id": 2, "post_id": 0, "body": "My comment"}]
+# }
