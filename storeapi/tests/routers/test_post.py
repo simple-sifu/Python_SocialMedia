39366d2e28442ac76b8934cf2856eb92c69b7fb2
Tommy Han <tommy.han@ownbackup.com>
2023-12-24 23:41:31 -0500

    create commentt

--- storeapi/tests/routers/test_post.py
+++ storeapi/tests/routers/test_post.py
@@ -3,11 +3,21 @@ from httpx import AsyncClient
 
 async def create_post(body: str, async_client: AsyncClient) -> dict:
     response = await async_client.post("/post", json={"body": body})
-    return response.json
+    return response.json()
+
+async def create_comment(body: str, post_id: int, async_client: AsyncClient) -> dict:
+    response = await async_client.post(
+        "/comment", json={"body": body, "post_id": post_id}
+    )
+    return response.json()
 
 @pytest.fixture()
 async def created_post(async_client: AsyncClient):
     return await create_post("Test Post", async_client)
+
+@pytest.fixture()
+async def created_comment(async_client: AsyncClient, created_post: dict ):
+    return await create_comment("Test Comment", created_post["id"], async_client)
 
 @pytest.mark.anyio
 async def test_create_post(async_client: AsyncClient):
@@ -34,8 +44,23 @@ async def test_create_post_missing_data(async_client: AsyncClient):
 
 
 @pytest.mark.anyio
-async def test_all_posts(async_client: AsyncClient, created_post: dict):
+async def test_get_all_posts(async_client: AsyncClient, created_post: dict):
     response = await async_client.get('/post')
 
+    print("created_post =", created_post)
     assert response.status_code == 200
     assert response.json() == [created_post]
+
+
+@pytest.mark.anyio
+async def test_create_comment(async_client: AsyncClient, created_post: dict):
+    body = "Test Comment"
+
+    response = await async_client.post('/comment', json={"body": body, "post_id": created_post["id"]})
+
+    assert response.status_code == 201
+    assert {
+        "id": 0,
+        "body": body,
+        "post_id": created_post["id"]
+    }.items() <= response.json().items()
