5d8965dd60f408996298a374cb204cef3f036983
Tommy Han <tommy.han@ownbackup.com>
2023-12-24 22:09:02 -0500

    add async tests

---
+++ storeapi/tests/routers/test_post.py
@@ -0,0 +1,33 @@
+import pytest
+from httpx import AsyncClient
+
+async def create_post(body: str, async_client: AsyncClient) -> dict:
+    response = await async_client.post("/post", json={"body": body})
+    return response.json
+
+@pytest.fixture()
+async def created_post(async_client: AsyncClient):
+    return await create_post("Test Post", async_client)
+
+@pytest.mark.anyio
+async def test_create_post(async_client: AsyncClient):
+    body = "Test Post"
+
+    response = await async_client.post(
+        "/post",
+        json={"body": body}
+    )
+
+    assert response.status_code == 201
+    assert {"id": 0, "body": body}.items() <= response.json().items() 
+
+
+@pytest.mark.anyio
+async def test_create_post_missing_data(async_client: AsyncClient):
+
+    response = await async_client.post(
+        "/post",
+        json={}
+    )
+
+    assert response.status_code == 422
